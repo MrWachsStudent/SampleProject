b4b41c02dd6f6425818628d66fcb05217be2c1e3
Mr. Wachs <mrLWachs@gmail.com>
2022-03-04 09:55:04 -0600

    Sample GitHub Project

---
+++ src/sampleproject/SampleProject.java
@@ -0,0 +1,20 @@
+
+package sampleproject;
+
+/** 
+ * SampleProject.java - 
+ *
+ * @author Mr. Wachs 
+ * @since 4-Mar-2022 
+ */
+public class SampleProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("To see if it works!");
+    }
+
+}
